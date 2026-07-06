ec9cfaa96139dbc06c7014b0a2a78b5cb0125c8f
이경준 <dlrudwns0595@gmail.com>
2026-07-06 18:08:00 +0900

    Reduce next moon countdown emphasis

--- OneulDal/DesignSystem.swift
+++ OneulDal/DesignSystem.swift
@@ -30,7 +30,7 @@ enum MoonLayout {
     static let heroPhaseSubtitleTextSize: CGFloat = 22
     static let nextMoonTitleTextSize: CGFloat = 28
     static let nextMoonDateTextSize: CGFloat = 20
-    static let nextMoonCountdownTextSize: CGFloat = 30
+    static let nextMoonCountdownTextSize: CGFloat = 28
     static let nextMoonChevronOpacity: Double = 0.72
     static let nextMoonChevronSize: CGFloat = 26
     static let previewMoonThumbnailSize: CGFloat = 82
