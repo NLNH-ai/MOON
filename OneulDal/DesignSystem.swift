f79b5fd9416bf1b687f170748b915e360cf1978c
이경준 <dlrudwns0595@gmail.com>
2026-07-06 18:13:28 +0900

    Soften next moon chevron

--- OneulDal/DesignSystem.swift
+++ OneulDal/DesignSystem.swift
@@ -31,7 +31,7 @@ enum MoonLayout {
     static let nextMoonTitleTextSize: CGFloat = 28
     static let nextMoonDateTextSize: CGFloat = 20
     static let nextMoonCountdownTextSize: CGFloat = 28
-    static let nextMoonChevronOpacity: Double = 0.72
+    static let nextMoonChevronOpacity: Double = 0.66
     static let nextMoonChevronSize: CGFloat = 26
     static let previewMoonThumbnailSize: CGFloat = 82
     static let previewMoonThumbnailStrokeOpacity: Double = 0.20
