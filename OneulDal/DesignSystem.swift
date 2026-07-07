f6dc35ac39ab3ce67c8a4c5920525966ed3adf48
이경준 <dlrudwns0595@gmail.com>
2026-07-07 16:10:07 +0900

    Soften status metric text

--- OneulDal/DesignSystem.swift
+++ OneulDal/DesignSystem.swift
@@ -46,7 +46,7 @@ enum MoonLayout {
     static let tabSelectedOpacity: Double = 0.88
     static let tabNormalOpacity: Double = 0.54
     static let tabBackgroundOpacity: Double = 0.62
-    static let statusMetricTextSize: CGFloat = 18
+    static let statusMetricTextSize: CGFloat = 17
     static let statusMetricIconSize: CGFloat = 22
     static let statusMetricIconFrameSize: CGFloat = 30
     static let statusMetricContentSpacing: CGFloat = 7
